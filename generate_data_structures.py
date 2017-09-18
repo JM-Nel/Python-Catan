bccca7dcb7679b8e38239f4bf08a918bc9b535e5
JM Nel <jmnel.dev@gmail.com>
2017-09-18 21:25:20 +0200

    Added the generate_data_structures.py file

---
+++ generate_data_structures.py
@@ -0,0 +1,448 @@
+
+
+erfs = {
+    '1': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['1_4','1_5'],
+    'Harbour': None
+    },
+
+    '2': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['2_5','2_6'],
+    'Harbour': None
+    },
+    '3': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['3_6','3_7'],
+    'Harbour': None
+    },
+    '4': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['1_4','4_8'],
+    'Harbour': None
+    },
+    '5': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['1_5','2_5','5_9'],
+    'Harbour': None
+    },
+    '6': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['2_6','3_6','6_10'],
+    'Harbour': None
+    },
+    '7': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['3_7','7_11'],
+    'Harbour': None
+    },
+    '8': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['4_8','8_12','8_13'],
+    'Harbour': None
+    },
+    '9': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['5_9','9_13','9_14'],
+    'Harbour': None
+    },
+    '10': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['6_10','10_14','10_15'],
+    'Harbour': None
+    },
+    '11': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['7_11','11_15','11_16'],
+    'Harbour': None
+    },
+    '12': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['8_12','12_17'],
+    'Harbour': None
+    },
+    '13': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['8_13','9_13','13_18'],
+    'Harbour': None
+    },
+    '14': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['9_14','10_14', '14_19'],
+    'Harbour': None
+    },
+    '15': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['10_15','11_15','15_20'],
+    'Harbour': None
+    },
+    '16': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['11_16','16_21'],
+    'Harbour': None
+    },
+    '17': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['12_17','17_22', '17_23'],
+    'Harbour': None
+    },
+    '18': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['13_18','18_23','18_24'],
+    'Harbour': None
+    },
+    '19': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['14_19','19_24','19_25'],
+    'Harbour': None
+    },
+    '20': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['15_20','20_25','20_26'],
+    'Harbour': None
+    },
+    '21': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['16_21','21_26','21_27'],
+    'Harbour': None
+    },
+    '22': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['17_22','22_28'],
+    'Harbour': None
+    },
+    '23': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['17_23','18_23','23_29'],
+    'Harbour': None
+    },
+    '24': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['18_24','19_24','24_30'],
+    'Harbour': None
+    },
+    '25': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['19_25', '20_25', '25_31'],
+    'Harbour': None
+    },
+    '26': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['20_26', '21_26','26_32'],
+    'Harbour': None
+    },
+    '27': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['21_27', '27_33'],
+    'Harbour': None
+    },
+    '28': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['22_28', '28_34'],
+    'Harbour': None
+    },
+    '29': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['23_29', '29_34', '29_35'],
+    'Harbour': None
+    },
+    '30': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['24_30', '30_35', '30_36'],
+    'Harbour': None
+    },
+    '31': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['25_31', '31_36', '31_37'],
+    'Harbour': None
+    },
+    '32': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['26_32','32_37', '32_38'],
+    'Harbour': None
+    },
+    '33': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['27_33', '33_38'],
+    'Harbour': None
+    },
+    '34': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['28_34', '29_34', '34_39'],
+    'Harbour': None
+    },
+    '35': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['29_35', '30_35', '35_40'],
+    'Harbour': None
+    },
+    '36': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['30_36', '31_36', '36_41'],
+    'Harbour': None
+    },
+    '37': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['31_37','32_37', '37_42'],
+    'Harbour': None
+    },
+    '38': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['32_38', '33_38', '38_43'],
+    'Harbour': None
+    },
+    '39': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['34_39','39_44'],
+    'Harbour': None
+    },
+    '40': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['35_40', '40_44', '40_45'],
+    'Harbour': None
+    },
+    '41': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['36_41', '41_45','41_46'],
+    'Harbour': None
+    },
+    '42': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['37_42', '42_46', '42_47'],
+    'Harbour': None
+    },
+    '43': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['38_43', '43_47'],
+    'Harbour': None
+    },
+    '44': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['39_44', '40_44', '44_48'],
+    'Harbour': None
+    },
+    '45': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['40_45', '41_45', '45_49', ],
+    'Harbour': None
+    },
+    '46': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['41_46', '42_46','46_50'],
+    'Harbour': None
+    },
+    '47': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['42_47','43_47', '47_51'],
+    'Harbour': None
+    },
+    '48': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['44_48', '48_52'],
+    'Harbour': None},
+
+    '49': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['45_49','49_52','49_53'],
+    'Harbour': None
+    },
+    '50': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['46_50', '50_53', '50_54'],
+    'Harbour': None
+    },
+    '51': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['47_51', '51_54'],
+    'Harbour': None
+    },
+    '52': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['48_52', '49_52'],
+    'Harbour': None
+    },
+    '53': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['49_53', '50_53'],
+    'Harbour': None
+    },
+    '54': {
+    'house': None,
+    'city': None,
+    'connected_roads': ['50_54','51_54'],
+    'Harbour': None
+    }
+}
+
+
+
+roads_dic = {
+
+
+'1_4': ['1', '4'],
+'1_5': ['1', '5'],
+'2_5': ['2', '5'],
+'2_6': ['2', '6'],
+'3_6': ['3', '6'],
+'3_7': ['3', '7'],
+'4_8': ['4', '8'],
+'5_9': ['5', '9'],
+'6_10': ['6', '10'],
+'7_11': ['7', '11'],
+'8_12': ['8', '12'],
+'8_13': ['8', '13'],
+'9_13': ['9', '13'],
+'9_14': ['9', '14'],
+'10_14': ['10', '14'],
+'10_15': ['10', '15'],
+'11_15': ['11', '15'],
+'11_16': ['11', '16'],
+'12_17': ['12', '17'],
+'13_18': ['13', '18'],
+'14_19': ['14', '19'],
+'15_20': ['15', '20'],
+'16_21': ['16', '21'],
+'17_22': ['17', '22'],
+'17_23': ['17', '23'],
+'18_23': ['18', '23'],
+'18_24': ['18', '24'],
+'19_24': ['19', '24'],
+'19_25': ['19', '25'],
+'20_25': ['20', '25'],
+'20_26': ['20', '26'],
+'21_26': ['21', '26'],
+'21_27': ['21', '27'],
+'22_28': ['22', '28'],
+'23_29': ['23', '29'],
+'24_30': ['24', '30'],
+'25_31': ['25', '31'],
+'26_32': ['26', '32'],
+'27_33': ['27', '33'],
+'28_34': ['28', '34'],
+'29_34': ['29', '34'],
+'29_35': ['29', '35'],
+'30_35': ['30', '35'],
+'30_36': ['30', '36'],
+'31_36': ['31', '36'],
+'31_37': ['31', '37'],
+'32_38': ['32', '38'],
+'33_38': ['33', '38'],
+'34_39': ['34', '39'],
+'35_40': ['35', '40'],
+'36_41': ['36', '41'],
+'37_42': ['37', '42'],
+'38_43': ['38', '43'],
+'39_44': ['39', '44'],
+'40_44': ['40', '44'],
+'40_45': ['40', '45'],
+'41_45': ['41', '45'],
+'41_46': ['41', '46'],
+'42_46': ['42', '46'],
+'42_47': ['42', '47'],
+'43_47': ['43', '47'],
+'44_48': ['44', '48'],
+'45_49': ['45', '49'],
+'46_50': ['46', '50'],
+'47_51': ['47', '51'],
+'48_52': ['48', '52'],
+'49_52': ['49', '52'],
+'49_53': ['49', '53'],
+'50_54': ['50', '54'],
+'50_53': ['50', '53'],
+'51_54': ['51', '54']}
+
+
+def generate_road_code():
+    for main_key in roads_dic:
+        connected = []
+        lines = []
+        for main_erf in roads_dic[main_key]:
+            for check_key in roads_dic:
+                if main_key == check_key:
+                    continue
+                for check_erf in roads_dic[check_key]:
+                    if main_erf == check_erf:
+                        connected.append(check_key)
+        lines.append("'%s': {'built': None, 'connected_roads': %s},"  % (main_key, connected))
+        for line in sorted(lines):
+            print line
+
+#'1_4': {'built': None, 'connected_roads':  ['1_5', '4_8']},
+
+def generate_plot_code():
+    for i in range(1, 55):
+        connected_roads = []
+        for road_key in roads_dic:
+            for num in roads_dic[road_key]:
+                if num == str(i):
+                    connected_roads.append(road_key)
+        connected_houses = []
+        for road in connected_roads:
+            for house in roads_dic[road]:
+                if house != str(i):
+                    connected_houses.append(house)
+
+
+
+        print "    '%s':{ \n        'house': None,\n        'city': None,\n        'connected_roads': %s,\n        'connected_houses': %s, \n        'harbour':None\n        },\n" %(i, connected_roads, connected_houses)
+
+
+generate_plot_code()
+generate_road_code()
+
+
+
