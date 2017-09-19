923d26f3b7cc3addf96fd063632570dcace6b7f0
JM Nel <jmnel.dev@gmail.com>
2017-09-19 22:12:15 +0200

    Added the check_if_building_through_other_player_house function Changed added connected plots to the roads data structure

--- Main.py
+++ Main.py
@@ -531,76 +531,431 @@ tiles = {
           'plots': ['42', '46', '47', '50', '51', '54']}}
 
 roads = {
-    '1_4': {'built': None, 'connected_roads': ['1_5', '4_8']},
-    '1_5': {'built': None, 'connected_roads': ['1_4', '2_5', '5_9']},
-    '2_5': {'built': None, 'connected_roads': ['2_6', '1_5', '5_9']},
-    '2_6': {'built': None, 'connected_roads': ['2_5', '3_6', '6_10']},
-    '3_6': {'built': None, 'connected_roads': ['3_7', '2_6', '6_10']},
-    '3_7': {'built': None, 'connected_roads': ['3_6', '7_11']},
-    '4_8': {'built': None, 'connected_roads': ['1_4', '8_12', '8_13']},
-    '5_9': {'built': None, 'connected_roads': ['1_5', '2_5', '9_13', '9_14']},
-    '6_10': {'built': None, 'connected_roads': ['3_6', '2_6', '10_14', '10_15']},
-    '7_11': {'built': None, 'connected_roads': ['3_7', '11_16', '11_15']},
-    '8_12': {'built': None, 'connected_roads': ['8_13', '4_8', '12_17']},
-    '8_13': {'built': None, 'connected_roads': ['8_12', '4_8', '13_18', '9_13']},
-    '9_13': {'built': None, 'connected_roads': ['5_9', '9_14', '8_13', '13_18']},
-    '9_14': {'built': None, 'connected_roads': ['5_9', '9_13', '14_19', '10_14']},
-    '10_14': {'built': None, 'connected_roads': ['10_15', '6_10', '14_19', '9_14']},
-    '10_15': {'built': None, 'connected_roads': ['10_14', '6_10', '15_20', '11_15']},
-    '11_15': {'built': None, 'connected_roads': ['7_11', '11_16', '10_15', '15_20']},
-    '11_16': {'built': None, 'connected_roads': ['7_11', '11_15', '16_21']},
-    '12_17': {'built': None, 'connected_roads': ['8_12', '17_23', '17_22']},
-    '13_18': {'built': None, 'connected_roads': ['8_13', '9_13', '18_24', '18_23']},
-    '14_19': {'built': None, 'connected_roads': ['10_14', '9_14', '19_24', '19_25']},
-    '15_20': {'built': None, 'connected_roads': ['10_15', '11_15', '20_26', '20_25']},
-    '16_21': {'built': None, 'connected_roads': ['11_16', '21_27', '21_26']},
-    '17_22': {'built': None, 'connected_roads': ['17_23', '12_17', '22_28']},
-    '17_23': {'built': None, 'connected_roads': ['12_17', '17_22', '23_29', '18_23']},
-    '18_23': {'built': None, 'connected_roads': ['13_18', '18_24', '23_29', '17_23']},
-    '18_24': {'built': None, 'connected_roads': ['13_18', '18_23', '19_24', '24_30']},
-    '19_24': {'built': None, 'connected_roads': ['19_25', '14_19', '18_24', '24_30']},
-    '19_25': {'built': None, 'connected_roads': ['19_24', '14_19', '25_31', '20_25']},
-    '20_25': {'built': None, 'connected_roads': ['20_26', '15_20', '19_25', '25_31']},
-    '20_26': {'built': None, 'connected_roads': ['20_25', '15_20', '21_26', '26_32']},
-    '21_26': {'built': None, 'connected_roads': ['21_27', '16_21', '20_26', '26_32']},
-    '21_27': {'built': None, 'connected_roads': ['21_26', '16_21', '27_33']},
-    '22_28': {'built': None, 'connected_roads': ['17_22', '28_34']},
-    '23_29': {'built': None, 'connected_roads': ['17_23', '18_23', '29_34', '29_35']},
-    '24_30': {'built': None, 'connected_roads': ['19_24', '18_24', '30_35', '30_36']},
-    '25_31': {'built': None, 'connected_roads': ['19_25', '20_25', '31_37', '31_36']},
-    '26_32': {'built': None, 'connected_roads': ['21_26', '20_26', '32_38']},
-    '27_33': {'built': None, 'connected_roads': ['21_27', '33_38']},
-    '28_34': {'built': None, 'connected_roads': ['22_28', '34_39', '29_34']},
-    '29_34': {'built': None, 'connected_roads': ['23_29', '29_35', '28_34', '34_39']},
-    '29_35': {'built': None, 'connected_roads': ['23_29', '29_34', '35_40', '30_35']},
-    '30_35': {'built': None, 'connected_roads': ['24_30', '30_36', '35_40', '29_35']},
-    '30_36': {'built': None, 'connected_roads': ['24_30', '30_35', '31_36', '36_41']},
-    '31_36': {'built': None, 'connected_roads': ['25_31', '31_37', '30_36', '36_41']},
-    '31_37': {'built': None, 'connected_roads': ['25_31', '31_36', '37_42']},
-    '32_38': {'built': None, 'connected_roads': ['26_32', '38_43', '33_38']},
-    '33_38': {'built': None, 'connected_roads': ['27_33', '32_38', '38_43']},
-    '34_39': {'built': None, 'connected_roads': ['28_34', '29_34', '39_44']},
-    '35_40': {'built': None, 'connected_roads': ['30_35', '29_35', '40_44', '40_45']},
-    '36_41': {'built': None, 'connected_roads': ['31_36', '30_36', '41_45', '41_46']},
-    '37_42': {'built': None, 'connected_roads': ['31_37', '42_47', '42_46']},
-    '38_43': {'built': None, 'connected_roads': ['32_38', '33_38', '43_47']},
-    '39_44': {'built': None, 'connected_roads': ['34_39', '44_48', '40_44']},
-    '40_44': {'built': None, 'connected_roads': ['35_40', '40_45', '44_48', '39_44']},
-    '40_45': {'built': None, 'connected_roads': ['35_40', '40_44', '41_45', '45_49']},
-    '41_45': {'built': None, 'connected_roads': ['41_46', '36_41', '40_45', '45_49']},
-    '41_46': {'built': None, 'connected_roads': ['41_45', '36_41', '46_50', '42_46']},
-    '42_46': {'built': None, 'connected_roads': ['37_42', '42_47', '46_50', '41_46']},
-    '42_47': {'built': None, 'connected_roads': ['37_42', '42_46', '43_47', '47_51']},
-    '43_47': {'built': None, 'connected_roads': ['38_43', '47_51', '42_47']},
-    '44_48': {'built': None, 'connected_roads': ['39_44', '40_44', '48_52']},
-    '45_49': {'built': None, 'connected_roads': ['41_45', '40_45', '49_52', '49_53']},
-    '46_50': {'built': None, 'connected_roads': ['41_46', '42_46', '50_54', '50_53']},
-    '47_51': {'built': None, 'connected_roads': ['43_47', '42_47', '51_54']},
-    '48_52': {'built': None, 'connected_roads': ['44_48', '49_52']},
-    '49_52': {'built': None, 'connected_roads': ['49_53', '45_49', '48_52']},
-    '49_53': {'built': None, 'connected_roads': ['49_52', '45_49', '50_53']},
-    '50_54': {'built': None, 'connected_roads': ['46_50', '50_53', '51_54']},
-    '51_54': {'built': None, 'connected_roads': ['47_51', '50_54']},
+    '1_4': {
+        'built': None,
+        'connected_roads': ['1_5', '4_8'],
+        'connected_plots': ['1', '4']
+    },
+
+    '1_5': {
+        'built': None,
+        'connected_roads': ['1_4', '2_5', '5_9'],
+        'connected_plots': ['1', '5']
+    },
+
+    '2_5': {
+        'built': None,
+        'connected_roads': ['2_6', '1_5', '5_9'],
+        'connected_plots': ['2', '5']
+    },
+
+    '2_6': {
+        'built': None,
+        'connected_roads': ['2_5', '3_6', '6_10'],
+        'connected_plots': ['2', '6']
+    },
+
+    '3_6': {
+        'built': None,
+        'connected_roads': ['3_7', '2_6', '6_10'],
+        'connected_plots': ['3', '6']
+    },
+
+    '3_7': {
+        'built': None,
+        'connected_roads': ['3_6', '7_11'],
+        'connected_plots': ['3', '7']
+    },
+
+    '4_8': {
+        'built': None,
+        'connected_roads': ['1_4', '8_12', '8_13'],
+        'connected_plots': ['4', '8']
+    },
+
+    '5_9': {
+        'built': None,
+        'connected_roads': ['1_5', '2_5', '9_13', '9_14'],
+        'connected_plots': ['5', '9']
+    },
+
+    '6_10': {
+        'built': None,
+        'connected_roads': ['3_6', '2_6', '10_14', '10_15'],
+        'connected_plots': ['6', '10']
+    },
+
+    '7_11': {
+        'built': None,
+        'connected_roads': ['3_7', '11_16', '11_15'],
+        'connected_plots': ['7', '11']
+    },
+
+    '8_12': {
+        'built': None,
+        'connected_roads': ['8_13', '4_8', '12_17'],
+        'connected_plots': ['8', '12']
+    },
+
+    '8_13': {
+        'built': None,
+        'connected_roads': ['8_12', '4_8', '13_18', '9_13'],
+        'connected_plots': ['8', '13']
+    },
+
+    '9_13': {
+        'built': None,
+        'connected_roads': ['5_9', '9_14', '8_13', '13_18'],
+        'connected_plots': ['9', '13']
+    },
+
+    '9_14': {
+        'built': None,
+        'connected_roads': ['5_9', '9_13', '14_19', '10_14'],
+        'connected_plots': ['9', '14']
+    },
+
+    '10_14': {
+        'built': None,
+        'connected_roads': ['10_15', '6_10', '14_19', '9_14'],
+        'connected_plots': ['10', '14']
+    },
+
+    '10_15': {
+        'built': None,
+        'connected_roads': ['10_14', '6_10', '15_20', '11_15'],
+        'connected_plots': ['10', '15']
+    },
+
+    '11_15': {
+        'built': None,
+        'connected_roads': ['7_11', '11_16', '10_15', '15_20'],
+        'connected_plots': ['11', '15']
+    },
+
+    '11_16': {
+        'built': None,
+        'connected_roads': ['7_11', '11_15', '16_21'],
+        'connected_plots': ['11', '16']
+    },
+
+    '12_17': {
+        'built': None,
+        'connected_roads': ['8_12', '17_23', '17_22'],
+        'connected_plots': ['12', '17']
+    },
+
+    '13_18': {
+        'built': None,
+        'connected_roads': ['8_13', '9_13', '18_24', '18_23'],
+        'connected_plots': ['13', '18']
+    },
+
+    '14_19': {
+        'built': None,
+        'connected_roads': ['10_14', '9_14', '19_24', '19_25'],
+        'connected_plots': ['14', '19']
+    },
+
+    '15_20': {
+        'built': None,
+        'connected_roads': ['10_15', '11_15', '20_26', '20_25'],
+        'connected_plots': ['15', '20']
+    },
+
+    '16_21': {
+        'built': None,
+        'connected_roads': ['11_16', '21_27', '21_26'],
+        'connected_plots': ['16', '21']
+    },
+
+    '17_22': {
+        'built': None,
+        'connected_roads': ['17_23', '12_17', '22_28'],
+        'connected_plots': ['17', '22']
+    },
+
+    '17_23': {
+        'built': None,
+        'connected_roads': ['12_17', '17_22', '23_29', '18_23'],
+        'connected_plots': ['17', '23']
+    },
+
+    '18_23': {
+        'built': None,
+        'connected_roads': ['13_18', '18_24', '23_29', '17_23'],
+        'connected_plots': ['18', '23']
+    },
+
+    '18_24': {
+        'built': None,
+        'connected_roads': ['13_18', '18_23', '19_24', '24_30'],
+        'connected_plots': ['18', '24']
+    },
+
+    '19_24': {
+        'built': None,
+        'connected_roads': ['19_25', '14_19', '18_24', '24_30'],
+        'connected_plots': ['19', '24']
+    },
+
+    '19_25': {
+        'built': None,
+        'connected_roads': ['19_24', '14_19', '25_31', '20_25'],
+        'connected_plots': ['19', '25']
+    },
+
+    '20_25': {
+        'built': None,
+        'connected_roads': ['20_26', '15_20', '19_25', '25_31'],
+        'connected_plots': ['20', '25']
+    },
+
+    '20_26': {
+        'built': None,
+        'connected_roads': ['20_25', '15_20', '21_26', '26_32'],
+        'connected_plots': ['20', '26']
+    },
+
+    '21_26': {
+        'built': None,
+        'connected_roads': ['21_27', '16_21', '20_26', '26_32'],
+        'connected_plots': ['21', '26']
+    },
+
+    '21_27': {
+        'built': None,
+        'connected_roads': ['21_26', '16_21', '27_33'],
+        'connected_plots': ['21', '27']
+    },
+
+    '22_28': {
+        'built': None,
+        'connected_roads': ['17_22', '28_34'],
+        'connected_plots': ['22', '28']
+    },
+
+    '23_29': {
+        'built': None,
+        'connected_roads': ['17_23', '18_23', '29_34', '29_35'],
+        'connected_plots': ['23', '29']
+    },
+
+    '24_30': {
+        'built': None,
+        'connected_roads': ['19_24', '18_24', '30_35', '30_36'],
+        'connected_plots': ['24', '30']
+    },
+
+    '25_31': {
+        'built': None,
+        'connected_roads': ['19_25', '20_25', '31_37', '31_36'],
+        'connected_plots': ['25', '31']
+    },
+
+    '26_32': {
+        'built': None,
+        'connected_roads': ['21_26', '20_26', '32_38'],
+        'connected_plots': ['26', '32']
+    },
+
+    '27_33': {
+        'built': None,
+        'connected_roads': ['21_27', '33_38'],
+        'connected_plots': ['27', '33']
+    },
+
+    '28_34': {
+        'built': None,
+        'connected_roads': ['22_28', '34_39', '29_34'],
+        'connected_plots': ['28', '34']
+    },
+
+    '29_34': {
+        'built': None,
+        'connected_roads': ['23_29', '29_35', '28_34', '34_39'],
+        'connected_plots': ['29', '34']
+    },
+
+    '29_35': {
+        'built': None,
+        'connected_roads': ['23_29', '29_34', '35_40', '30_35'],
+        'connected_plots': ['29', '35']
+    },
+
+    '30_35': {
+        'built': None,
+        'connected_roads': ['24_30', '30_36', '35_40', '29_35'],
+        'connected_plots': ['30', '35']
+    },
+
+    '30_36': {
+        'built': None,
+        'connected_roads': ['24_30', '30_35', '31_36', '36_41'],
+        'connected_plots': ['30', '36']
+    },
+
+    '31_36': {
+        'built': None,
+        'connected_roads': ['25_31', '31_37', '30_36', '36_41'],
+        'connected_plots': ['31', '36']
+    },
+
+    '31_37': {
+        'built': None,
+        'connected_roads': ['25_31', '31_36', '37_42'],
+        'connected_plots': ['31', '37']
+    },
+
+    '32_38': {
+        'built': None,
+        'connected_roads': ['26_32', '38_43', '33_38'],
+        'connected_plots': ['32', '38']
+    },
+
+    '33_38': {
+        'built': None,
+        'connected_roads': ['27_33', '32_38', '38_43'],
+        'connected_plots': ['33', '38']
+    },
+
+    '34_39': {
+        'built': None,
+        'connected_roads': ['28_34', '29_34', '39_44'],
+        'connected_plots': ['34', '39']
+    },
+
+    '35_40': {
+        'built': None,
+        'connected_roads': ['30_35', '29_35', '40_44', '40_45'],
+        'connected_plots': ['35', '40']
+    },
+
+    '36_41': {
+        'built': None,
+        'connected_roads': ['31_36', '30_36', '41_45', '41_46'],
+        'connected_plots': ['36', '41']
+    },
+
+    '37_42': {
+        'built': None,
+        'connected_roads': ['31_37', '42_47', '42_46'],
+        'connected_plots': ['37', '42']
+    },
+
+    '38_43': {
+        'built': None,
+        'connected_roads': ['32_38', '33_38', '43_47'],
+        'connected_plots': ['38', '43']
+    },
+
+    '39_44': {
+        'built': None,
+        'connected_roads': ['34_39', '44_48', '40_44'],
+        'connected_plots': ['39', '44']
+    },
+
+    '40_44': {
+        'built': None,
+        'connected_roads': ['35_40', '40_45', '44_48', '39_44'],
+        'connected_plots': ['40', '44']
+    },
+
+    '40_45': {
+        'built': None,
+        'connected_roads': ['35_40', '40_44', '41_45', '45_49'],
+        'connected_plots': ['40', '45']
+    },
+
+    '41_45': {
+        'built': None,
+        'connected_roads': ['41_46', '36_41', '40_45', '45_49'],
+        'connected_plots': ['41', '45']
+    },
+
+    '41_46': {
+        'built': None,
+        'connected_roads': ['41_45', '36_41', '46_50', '42_46'],
+        'connected_plots': ['41', '46']
+    },
+
+    '42_46': {
+        'built': None,
+        'connected_roads': ['37_42', '42_47', '46_50', '41_46'],
+        'connected_plots': ['42', '46']
+    },
+
+    '42_47': {
+        'built': None,
+        'connected_roads': ['37_42', '42_46', '43_47', '47_51'],
+        'connected_plots': ['42', '47']
+    },
+
+    '43_47': {
+        'built': None,
+        'connected_roads': ['38_43', '47_51', '42_47'],
+        'connected_plots': ['43', '47']
+    },
+
+    '44_48': {
+        'built': None,
+        'connected_roads': ['39_44', '40_44', '48_52'],
+        'connected_plots': ['44', '48']
+    },
+
+    '45_49': {
+        'built': None,
+        'connected_roads': ['41_45', '40_45', '49_52', '49_53'],
+        'connected_plots': ['45', '49']
+    },
+
+    '46_50': {
+        'built': None,
+        'connected_roads': ['41_46', '42_46', '50_54', '50_53'],
+        'connected_plots': ['46', '50']
+    },
+
+    '47_51': {
+        'built': None,
+        'connected_roads': ['43_47', '42_47', '51_54'],
+        'connected_plots': ['47', '51']
+    },
+
+    '48_52': {
+        'built': None,
+        'connected_roads': ['44_48', '49_52'],
+        'connected_plots': ['48', '52']
+    },
+
+    '49_52': {
+        'built': None,
+        'connected_roads': ['49_53', '45_49', '48_52'],
+        'connected_plots': ['49', '52']
+    },
+
+    '49_53': {
+        'built': None,
+        'connected_roads': ['49_52', '45_49', '50_53'],
+        'connected_plots': ['49', '53']
+    },
+
+    '50_54': {
+        'built': None,
+        'connected_roads': ['46_50', '50_53', '51_54'],
+        'connected_plots': ['50', '54']
+    },
+
+    '50_53': {
+        'built': None,
+        'connected_roads': ['46_50', '50_54', '49_53'],
+        'connected_plots': ['50', '53']
+    },
+
+    '51_54': {
+        'built': None,
+        'connected_roads': ['47_51', '50_54'],
+        'connected_plots': ['51', '54']
+    }
 }
 
 
@@ -629,9 +984,17 @@ def no_house_nearby(plot):
 
 def not_build_through_other_player_house(road, player_colour):
     for connected_road in road[road]['connected_roads']:
-        if
-    connected_road =
-    plot_to_check =
+        if roads[connected_road]['built'] == player_colour:
+            true_connected_road = connected_road
+            break
+    for plot_connected_to_root_road in roads[true_connected_road]['connected_plots']:
+        for plot_connected_to_proposed_road in roads[road]['connected_plots']:
+            if plot_connected_to_proposed_road == plot_connected_to_root_road:
+                check_plot = plot_connected_to_proposed_road
+                break
+
+    if plots[check_plot]['house'] == player_colour or plots[check_plot]['house'] == None or plots[check_plot]['city'] == player_colour _
+    or plots[check_plot]['city'] == None
 
 def can_build_road_here(road, player_colour):
     if roads[road]['built'] == None and not_build_through_other_player_house(road, player_colour) and correct_road_connected_to_road(road, player_colour):
